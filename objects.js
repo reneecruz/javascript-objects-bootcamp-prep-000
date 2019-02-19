d35a4969159faeda9431a51a66fe7e5e2533ef52
Renée Cruz <reneebasscruz@gmail.com>
2019-02-19 19:55:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artistName: 'songTitle'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-     Object.assign({}, playlist { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi" })
+     Object.assign({}, { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi" })
 }
 
 function removeFromPlaylist(playlist, artistName) {
