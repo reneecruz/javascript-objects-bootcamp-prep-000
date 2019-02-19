af8247acc5502706c2fea7454c126a7467f52ac6
Renée Cruz <reneebasscruz@gmail.com>
2019-02-19 22:20:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 
 function removeFromPlaylist(playlist, artistName) {
-  delete artistName.songTitle;
+  delete playlist.artistName.songTitle;
   return playlist;
 }
