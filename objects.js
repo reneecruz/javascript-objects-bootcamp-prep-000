4bff6def79a921006194b042fef5718f33d20324
Renée Cruz <reneebasscruz@gmail.com>
2019-02-19 19:54:27 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {artistName: 'songTitle'};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+     Object.assign({}, playlist { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi" })
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
