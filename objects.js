caba64265abf65afcce4e6348fa1bc5cbbe96d68
Renée Cruz <reneebasscruz@gmail.com>
2019-02-19 22:30:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName["Alison"];
+  delete playlist.artistName["My Bloody Valentine"
+      -  "Phil Ochs"];
   return playlist;
 }
