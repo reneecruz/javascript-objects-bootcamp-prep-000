2c184796f120e06b27d147513469578bec5b8732
Renée Cruz <reneebasscruz@gmail.com>
2019-02-19 22:31:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName["Phil Ochs"];
+  delete playlist.artistName[];
   return playlist;
 }
