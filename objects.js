a9b74f7fd8f2c7169c7c9beb0a4166ad447659b5
Renée Cruz <reneebasscruz@gmail.com>
2019-02-19 22:30:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName["Slowdive"];
+  delete playlist.artistName["My Bloody Valentine"];
   return playlist;
 }
